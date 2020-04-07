1c2b8ced90af5bad68200661a804ba1c8583aa9b
sma <qw@qw.ru>
2020-04-07 23:58:48 +0300

    переписан класс ReadFromFile, теперь он читает из файла, класс Basis теперь его применяет

--- src/Basis.java
+++ src/Basis.java
@@ -3,20 +3,44 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Basis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         //задаем путь и имя входного файла
         String pathToInputFile = "E:\\Java\\";
         String nameOfInputFile = "inputFile";
 
-        System.out.println("Поместите файл под именем "+nameOfInputFile+", содержащий пример,  в каталог "+pathToInputFile+" и нажмите Enter");
+
+        System.out.println("Поместите файл под именем " + nameOfInputFile + ", содержащий пример,  в каталог " + pathToInputFile + " и нажмите Enter");
         Scanner scanner = new Scanner(System.in);
         String preInputEnter = scanner.nextLine();
+        scanner.close();
 
-        ReadFromFile out = new ReadFromFile(pathToInputFile,nameOfInputFile);
-        //out.setDirectoryToFile(pathToInputFile);
-        //out.setFileName(nameOfInputFile);
-        out.outPutWhatRead();
+
+       ReadFromFile lineFromFile = new ReadFromFile(pathToInputFile, nameOfInputFile);
+      // String gettedLine = lineFromFile.getLine();
+
+
+        System.out.println("То, что прочитано из файла: "+lineFromFile.getLineFromFile());
+
+       // System.out.println("DirFrFi: "+lineFromFile.getDirectoryToFile());
+        //System.out.println("Name File: "+lineFromFile.getFileName());
+        //System.out.println("File "+lineFromFile.getFile());
+
+        /*
+        String path = pathToInputFile + nameOfInputFile;
+        File file = new File(path);
+
+        Scanner scanner1 = new Scanner(file);
+        String line = scanner1.nextLine();
+        scanner1.close();
+        System.out.println("То, что прочитано из файла: " + line);
+        */
     }
+
+
+
 }
+
+
+
 
